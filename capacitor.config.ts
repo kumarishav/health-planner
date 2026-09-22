877ea1300410c6506cc92a3a24656cc309309eb9
Rishav Kumar <86093908+kumarishav@users.noreply.github.com>
2026-09-22 17:16:04 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.healthplanner',
+  appName: 'Health Planner',
+  webDir: 'www'
+};
+
+export default config;
