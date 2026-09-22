7b7075b9f85b046e820d032822d89d53f4d1b207
Rishav Kumar <86093908+kumarishav@users.noreply.github.com>
2026-09-22 16:11:40 +0530

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.healthplanner',
-  appName: 'Health Planner',
-  webDir: 'www'
-};
-
-export default config;
